4cfca61c3cf1ca9836807c1df26c8d2e5ebed218
profThiernesse <44463345+profThiernesse@users.noreply.github.com>
2019-02-09 08:34:51 +0100

    Update exo1_test.php

--- php-template/exo1_test.php
+++ php-template/exo1_test.php
@@ -1,15 +1,21 @@
 <?php
     include 'exo1.php';
 	
-    function test($a) {
-        if ($a === 42 && $b="azerty") {
-            echo("TECHIO> success true \r\n");
-        } 
-        else {
-            echo("TECHIO> success false  \r\n");
-            echo("TECHIO> message --channel Oops! Try Again ðŸž \r\n");
-        }
-    }
+	$_d=array(0,1,4,9,16,25);
 	
-    test($a);
+	if ($a === "42" && $b === 42 && $c === "Hello World!" && $d === $_d) {
+		echo("TECHIO> success true \r\n");
+		$contents = file_get_contents("exo1.php");
+		if (preg_match("#(for|while)#",$contents))
+			echo("TECHIO> message --channel Super! Une solution facilement adaptable. \r\n");
+		else if (preg_match("#array\(.+\)#",$contents))
+			echo("TECHIO> message --channel C'est déjà pas mal. Mais cette solution serait-il pratique si le vecteur était beaucoup plus grand. \r\n");
+		else
+			echo("TECHIO> message --channel La solution répond à la question mais il y a moyen de faire mieux.\r\n");
+	} 
+	else {
+		echo("TECHIO> success false  \r\n");
+		echo("TECHIO> message --channel Oops! Try Again ðŸž \r\n");
+	}
+
 ?>
